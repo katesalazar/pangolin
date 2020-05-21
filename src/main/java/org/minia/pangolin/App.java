a31958ea494daa142b242af650ffc78a98ff41a2
Kate <mercedes.catherine.salazar@gmail.com>
2020-05-21 20:28:12 +0200

    Add and use the Maven Assembly Plugin...
    ... for `jar-with-dependencies`.

--- src/main/java/org/minia/pangolin/App.java
+++ src/main/java/org/minia/pangolin/App.java
@@ -1,11 +1,13 @@
 package org.minia.pangolin;
 
 import lombok.extern.java.Log;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Log
-public class App 
-{
-    private static final String FIGLET = "\n" +
+public class App {
+
+    private static final String FIGLET_DEFAULT = "\n" +
             "                               _ _       \n" +
             " _ __   __ _ _ __   __ _  ___ | (_)_ __  \n" +
             "| '_ \\ / _` | '_ \\ / _` |/ _ \\| | | '_ \\ \n" +
@@ -13,7 +15,16 @@ public class App
             "| .__/ \\__,_|_| |_|\\__, |\\___/|_|_|_| |_|\n" +
             "|_|                |___/                 ";
 
+    private static final String FIGLET_THREEPOINT = "\n" +
+            " _  _  _  _  _ |. _\n" +
+            "|_)(_|| |(_|(_)||| |\n" +
+            "|         _|\n";
+
     public static void main(final String[] args) {
-        log.info(FIGLET);
+        log.info(FIGLET_THREEPOINT);
+
+        // REMOVE THIS LATER, it is used to ensure access to classes is NOT working
+        Pair tuple = new ImmutablePair(0,0);
+        System.out.println(tuple);
     }
 }
